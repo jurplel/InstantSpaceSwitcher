6561c3b43f6fbd5dd40b3a311424c5c252664db4
al1ych <decter49pro@gmail.com>
2026-06-11 23:53:54 +0300

    Soften non-instant speed curve

--- Sources/ISS/ISS.c
+++ Sources/ISS/ISS.c
@@ -68,11 +68,11 @@ static bool swipeFired = false;
 static double gestureSpeed = 2000.0;
 static const double nonInstantGestureVelocityFloor = 40.0;
 static const double dockGestureActivationVelocityFloor = 80.0;
-static const double nonInstantGestureVelocityLinearMultiplier = 5.0;
+static const double nonInstantGestureVelocityLinearMultiplier = 1.5;
 static const double nonInstantGestureVelocityQuadraticMultiplier = 0.1;
 static const double instantGestureVelocity = 2000.0;
 static const double nonInstantGestureVelocityCeiling = 1999.0;
-static const double nonInstantGestureProgress = 0.35;
+static const double nonInstantGestureProgress = 0.25;
 
 static ISSSwitchCallback switchCallback = NULL;
 
